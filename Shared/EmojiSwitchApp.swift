19ef2f89b3652cf247ecbb891f76ba8cf52148cb
Imran Razak <Imranrazak1@icloud.com>
2021-11-10 17:05:50 +0000

    Initial Commit

---
+++ Shared/EmojiSwitchApp.swift
@@ -0,0 +1,17 @@
+//
+//  EmojiSwitchApp.swift
+//  Shared
+//
+//  Created by Imran razak on 10/11/2021.
+//
+
+import SwiftUI
+
+@main
+struct EmojiSwitchApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
